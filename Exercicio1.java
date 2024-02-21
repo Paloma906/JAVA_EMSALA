fff6be76f9e347d29f0b339a771ed5f74c5544e5
Paloma906 <128625040+Paloma906@users.noreply.github.com>
2024-02-21 20:23:46 -0300

    Update Exercicio1.java

--- Exercicio1.java
+++ Exercicio1.java
@@ -20,7 +20,7 @@ public class Exercicio1 {
     public static void main(String[] args) {
         
         Scanner sc = new Scanner(System.in);
-        int senha = 0;
+        int senha ;
         
         
         
