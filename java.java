34464a0a336fba292dbad037d1f26eab919dec2c
Paloma906 <128625040+Paloma906@users.noreply.github.com>
2024-03-20 16:15:05 -0300

    Add files via upload

---
+++ java.java
@@ -0,0 +1,36 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Conta;
+
+/**
+ *
+ * @author Aluno
+ */
+public class java {
+    private String nome;
+    private int numero;
+    private double saldo;
+    
+    public String setNome(){
+    return nome;
+    }
+    
+    public String getNome(){
+    return nome;
+    }
+    
+    public double getNumero(){
+    return numero;
+    }
+    
+    public double getSaldo(){
+    return saldo;
+    }
+    
+    
+    
+    
+    
+}
